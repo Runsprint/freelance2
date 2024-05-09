ec6cd7f116552f5c2f9eb9965ef9455a4a66e7ec
Runsprint <gvantsakapanadze95@gmail.com>
2024-05-09 17:06:40 +0400

    last

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
   const updiv = document.getElementById('updiv')
   const svgwrapper = document.getElementById('svgwrapper')
   const svgdollar = document.getElementById('svgdollar')
-  const seconddiv = document.getElementById('seconddiv')
-  const secondsvg = document.getElementById('secondsvg')
   const  content =document.getElementById('content')
   const  firsth2 =document.getElementById('firsth2')
+ 
+  const secondsvg = document.getElementById('secondsvg')
   secondsvg.style.display ='none'
-  seconddiv.style.display='none'
+
+
   const dollarimg  = document.getElementById('dollarimg')
   dollarimg.style.display ='none'
 
@@ -25,89 +26,105 @@ document.addEventListener('DOMContentLoaded', function() {
   const shape = document.querySelector('.shape');
  
   
-
-  
   window.onload = function() {
-    var content = document.getElementById("content");
-
-   
-  //   setTimeout(function() {
-  //     updiv.style.width="350px"
-  //     updiv.style.height="50px"
-  //     firsth2.style.fontSize="16px"
-  // }, 200);
-};
-  function triggerAnimation() {
-
-    svgWrapper.classList.add('active');
-    setTimeout(function() {   
-      document.getElementById('first').style.display = 'flex';
-    }, 3000); 
+    content.style.width = '370px';
+    content.style.height = '350px';
+    title.style.display ='flex'
+    firsth2.style.display='flex'
   }
 
+///first
+  function triggerAnimation() {
+    svgWrapper.classList.add('active');  
+  }
+  setTimeout(triggerAnimation, 1000);
+///////////second
+function secondpart(){
+  title.style.display ='none'
+  updiv.style.display ='none'
+  svgwrapper.style.display ='none'
+  svgdollar.style.display ='none'
+}
+setTimeout(secondpart, 6000);
+
+setTimeout(() => {
+secondsvg.style.display ='flex'
+secondp.style.display ='flex'
+}, 6500);
+
+setTimeout(() => {
+seccondh.style.display ='flex'
+  
+},6700);
+
+
+//display none second part
+setTimeout(() => {
+  secondsvg.style.transform = 'translateY(55px)';
+    secondsvg.style.transition = 'transform 1s ease-in-out';
+  },8600);
+ 
+  setTimeout(() => {
+    secondp.style.transform = 'translateY(-55px)';
+    secondp.style.transition = 'transform 1s ease-in-out'; 
+    seccondh.style.transform = 'translateY(-45px)';
+    seccondh.style.transition = 'transform 1s ease-in-out'; 
+  },8500);
+ 
+  
+  setTimeout(() => {
+    secondp.style.display ='none'
+    secondsvg.style.display ='none'
+    seccondh.style.display ='none' 
+  },8870);
+
+  ///additional 
   setTimeout(function (){
     svgWrapper.classList.remove('active');
+
   },7500)
 
   setTimeout(function (){
     svgWrapper.classList.add('active');
   },10000)
 
-  setTimeout(triggerAnimation, 1000);
-
-  function secondpart(){
-      title.style.display ='none'
-      updiv.style.display ='none'
-      // content.style.display ='none'
-      svgwrapper.style.display ='none'
-      svgdollar.style.display ='none'
-  }
-  setTimeout(secondpart, 6000);
-  setTimeout(() => {
-    seconddiv.style.display='flex' 
-    secondsvg.style.display ='flex'
- }, 6800);
- setTimeout(() => {
-  seccondh.style.display ='flex'
-      
- },7000);
- setTimeout(() => {
-  secondp.style.display ='flex'
- }, 7100);
   
 
-//   function thirdpart(){
-//     svgwrapper.style.display ='flex'
-//     title.style.display ='none'
-//     updiv.style.display ='none'
-//     svgdollar.style.display ='none'
-//     secondsvg.style.display ='none'
-//     seccondh.style.display ='none'
-//     secondp.style.display ='none'
-//     thirdp.style.display ='flex'
-//     dollarimg.style.display='flex'
-//     seconddiv.style.display='none'
-//     content.style.display ='flex'
-// }
+
+  
+
+  function thirdpart(){
+    title.style.display ='none'
+    updiv.style.display ='none'
+    svgdollar.style.display ='none'
+    secondsvg.style.display ='none'
+    seccondh.style.display ='none'
+    secondp.style.display ='none'
+    thirdp.style.display ='flex'
+    dollarimg.style.display='flex'
+    content.style.display ='flex'
+    svgwrapper.style.display ='flex'
+    svgwrapper.style.top ='151px'
+}
 setTimeout(thirdpart, 9500);
 });
 
-// setTimeout(() => {
-//   content.style.transition = 'transform 2s ease-in-out'; // Apply transition for smooth animation
-//   content.style.transform = 'translateY(-500px)';
-//   dollarimg.style.transition = 'transform 2s ease-in-out'; 
-//   dollarimg.style.transform = 'translateY(-500px)'; // Move content upward
-//   seconddiv.style.transform = 'translateY(-500px)';
-//   seconddiv.style.transition = 'transform 2s ease-in-out'; 
-//   thirdp.style.transform = 'translateY(-500px)';
-//   thirdp.style.transition = 'transform 2s ease-in-out'; 
-// }, 15730);
+setTimeout(() => {
+  content.style.transition = 'transform 1s ease-in-out'; 
+  content.style.transform = 'translateY(-700px)';
+  dollarimg.style.transition = 'transform 11s ease-in-out'; 
+  dollarimg.style.transform = 'translateY(-700px)'; 
+  seconddiv.style.transform = 'translateY(-500px)';
+  seconddiv.style.transition = 'transform 1s ease-in-out'; 
+  thirdp.style.transform = 'translateY(-700px)';
+  thirdp.style.transition = 'transform 1s ease-in-out'; 
+}, 13730);
 
 
-// setTimeout(() => {
-//   content.style.display = 'none'; 
-//   thirdp.style.display = 'none';
-//   dollarimg.style.display = 'none';
-//   seconddiv.style.display = 'none';
-// }, 17000);
+setTimeout(() => {
+  content.style.display = 'none'; 
+  thirdp.style.display = 'none';
+  dollarimg.style.display = 'none';
+  seconddiv.style.display = 'none';
+}, 15000);
 
